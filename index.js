203ab05bb449d37f41d0949ae1b0c462dce6b024
t1afranco <afranco@todo1.com>
2017-03-23 17:26:28 -0500

    prueba

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
-/*!
- * apiai
- * Copyright(c) 2015 http://api.ai/
- * Apache 2.0 Licensed
- */
-
  'use strict';
 
-// var http = require('http');
-// // var apiai = require("../module/apiai");
-// var apiai = require("apiai");
-
-// var app = apiai("ed66e9aa07fb47e7a9becbea1ccb7ad5");
-
-// var options = {
-//     sessionId: 'f97ae12b-471d-40ea-9d2f-c4b14aa19fa3'
-// };
-
-// var request = app.textRequest('Hello', options);
-
-// request.on('response', function(response) {
-//     console.log(response);
-// });
-
-// request.on('error', function(error) {
-//     console.log(error);
-// });
-
-// request.end();
 
 /*!
  * apiai
@@ -58,31 +31,78 @@
 // });
 // app.listen(3000);
 
-var express    = require("express");
-var app2 = express();
 
-var apiai = require('apiai');
-var app = apiai("ed66e9aa07fb47e7a9becbea1ccb7ad5");
 
-// app2.post("/",function(req,res){
-//   console.log("init server");
-//   var keyword = req.query.keyword;
-//   console.log(keyword);
+// var util = require('util');
+// // var apiai = require("../module/apiai");
+// var apiai = require("apiai");
+
+// var options = {
+//     // hostname: 'eap.api.ai',
+// };
+
+// var app = apiai("ed66e9aa07fb47e7a9becbea1ccb7ad5", options);
+
+// var event = {
+//     name: "network.connect",
+//     data: {
+//         param1: "param1 value",
+//     }
+// };
+
+// var options = {
+//     sessionId: '12345'
+// };
+
+// var request = app.eventRequest(event, options);
+
+// request.on('response', function(response) {
+//     console.log(util.inspect(response, false, null));
 // });
-// app2.listen(3000);
 
-var request = app.textRequest('Hello!', {
-  'sessionId': 123456
+// request.on('error', function(error) {
+//     console.log(error);
+// });
+
+// request.end();
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3400;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+var apiai = require("apiai");
+var api = apiai("ed66e9aa07fb47e7a9becbea1ccb7ad5");
+
+app.post('/api/users', function(req, res) {
+  var user_id = req.param('id');
+  var token = req.param('token');
+  var geo = req.param('geo');  
+
+  var options = {
+    sessionId: '12345'
+  };
+
+  var request = api.textRequest('Hello.', options);
+
+  request.on('response', function(response) {
+      console.log(response);
+      res.json(response);
+  });
+
+  request.on('error', function(error) {
+      console.log(error);
+  });
+
+  request.end();
+
+  //res.send(user_id + ' ' + token + ' ' + geo);
 });
 
-request.on('response', function(response) {
-  console.log(response);
-  console.log(response.result.fulfillment.speech);
-  console.log(response.result.metadata.intentName);
-});
+// routes will go here
 
-request.on('error', function(error) {
-  console.log(error);
-});
-
-request.end();
+// start the server
+app.listen(port);
+console.log('Server started! At http://localhost:' + port);
