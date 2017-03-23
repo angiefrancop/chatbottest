3673d8bc1fac8db897da9c9d388a8086b501124b
t1afranco <afranco@todo1.com>
2017-03-23 14:22:02 -0500

    ssss

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-var express    = require("express");
-var app = express();
-var apiai = require('apiai');
- 
-var app2 = apiai("ed66e9aa07fb47e7a9becbea1ccb7ad5");
+/*!
+ * apiai
+ * Copyright(c) 2015 http://api.ai/
+ * Apache 2.0 Licensed
+ */
 
-app.post("/",function(req,res){
-  var keyword = req.query.keyword;
-  console.log(keyword);
+'use strict';
 
-  var request = app2.textRequest('Hello.', {'sessionId': 123456});
+var http = require('http');
+// var apiai = require("../module/apiai");
+var apiai = require("apiai");
 
-  request.on('response', function(response) {
+var app = apiai("ed66e9aa07fb47e7a9becbea1ccb7ad5");
+
+var options = {
+    sessionId: '11111111'
+};
+
+var request = app.textRequest('Hello', options);
+
+request.on('response', function(response) {
     console.log(response);
-    res.json(response);
-  });
-
-  request.on('error', function(error) {
-    console.log(error);
-
-  });
 });
-app.listen(3000);
+
+request.on('error', function(error) {
+    console.log(error);
+});
+
+request.end();
